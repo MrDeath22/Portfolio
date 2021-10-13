9d7a2337e2e525040e260740765331c74ca64233
MrDeath22 <daller3017@gmail.com>
2021-10-13 20:10:15 +0200

    added js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,31 @@
+// Dark night mode
+
 let elBody = document.querySelector("body");
 let darkLightbtn = document.querySelector('.dark-light-btn');
 darkLightbtn.addEventListener('click', function() {
   elBody.classList.toggle('dark-content');
   console.log(adsa);
 })
+
+// Typing txt
+
+const texts = ['Web Developer', 'Student', 'Programmer']
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+  console.log(texts[count]);
+  document.querySelector(".typing").textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 200);
+})();
