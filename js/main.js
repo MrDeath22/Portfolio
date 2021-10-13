3e131652d4e0476dfd520840ec20a16da0e77e74
MrDeath22 <daller3017@gmail.com>
2021-10-13 06:48:48 +0200

    add

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let elBody = document.querySelector("body");
+let darkLightbtn = document.querySelector('.dark-light-btn');
+darkLightbtn.addEventListener('click', function() {
+  elBody.classList.toggle('dark-content');
+  console.log(adsa);
+})
